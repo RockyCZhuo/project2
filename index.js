cc1a52bc5b833b611906b2931452db1cf7a286b1
RockyCZhuo <107918941+RockyCZhuo@users.noreply.github.com>
2023-11-05 03:32:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ app.use("/", express.static("public"));
 let circleTeam = [];
 let crossTeam = [];
 let idType;
+let slotsCircleRecord = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+let slotsCrossRecord = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+let isCircleTurn = true;
 
 //creating the http server 
 let http = require("http");
@@ -28,6 +31,7 @@ io.sockets.on("connect", (socket) => {
       slotNum: 0
     }
     crossTeam.push(idType);
+    slotsCrossRecord[0] += 1;
   } else {
     idType = {
       id: socket.id,
@@ -37,7 +41,7 @@ io.sockets.on("connect", (socket) => {
       slotNum: 0
     }
     circleTeam.push(idType);
-    console.log(idType);
+    slotsCircleRecord[0] += 1;
   }
 
 
@@ -48,8 +52,25 @@ io.sockets.on("connect", (socket) => {
   io.sockets.emit("idTypeMsg", idType);
 
   socket.on("mouseData", (data) => {
-    //when client is moving mouse
+    //when client is moving mouse, data is received at server
     //console.log(data);
+    if (data.type == "circle") {
+      for (i = 0; i < circleTeam.length; i++) {
+        if (data.id == circleTeam[i].id) {
+          circleTeam[i].mouseX = data.x;
+          circleTeam[i].mouseY = data.y;
+        }
+      }
+      io.sockets.emit("updateTeamCircle",circleTeam);
+    } else if (data.type == "cross") {
+      for (j = 0; j < crossTeam.length; j++) {
+        if (data.id == crossTeam[j].id) {
+          crossTeam[j].mouseX = data.x;
+          crossTeam[j].mouseY = data.y;
+        }
+      }
+      io.sockets.emit("updateTeamCross",crossTeam);
+    }
 
 
 
@@ -57,6 +78,27 @@ io.sockets.on("connect", (socket) => {
     io.sockets.emit("mouseDataServer", data);
   })
 
+  socket.on("calculateSlots", (data) => {
+
+    if (data.type == "cross") {
+      slotsCrossRecord = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+      for (i = 0; i < crossTeam.length; i++) {
+        if (crossTeam[i].id == data.id) {
+          crossTeam[i].slotNum = data.newSlot;
+        }
+        slotsCrossRecord[crossTeam[i].slotNum] += 1;
+      }
+    } else if (data.type == "circle") {
+      slotsCircleRecord = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+      for (i = 0; i < circleTeam.length; i++) {
+        if (circleTeam[i].id == data.id) {
+          circleTeam[i].slotNum = data.newSlot;
+        }
+        slotsCircleRecord[circleTeam[i].slotNum] += 1;
+      }
+    }
+
+  })
 
 
 
@@ -64,27 +106,27 @@ io.sockets.on("connect", (socket) => {
   socket.on("disconnect", () => {
     let indexCircle = -1;
     let indexCross = -1;
-    console.log("Disconnection : ", socket.id);
+    //console.log("Disconnection : ", socket.id);
     //check the disconnected socket ID, remove it from current player list
     for (i = 0; i < circleTeam.length; i++) {
       if (circleTeam[i].id == socket.id) {
         indexCircle = i;
       }
     }
-    if (indexCircle == -1) {    //disconnected player is in team cross
-      for (j = 0; j < crossTeam.length; j++) {    
+    if (indexCircle == -1) {    //disconnected player is not in team circle
+      for (j = 0; j < crossTeam.length; j++) {     //disconnected player is in team cross
         if (crossTeam[j].id == socket.id) {
           indexCross = j;
         }
       }
-      console.log("Remove 1 cross");
+      slotsCrossRecord[crossTeam[indexCross].slotNum] -= 1;
       crossTeam.splice(indexCross, 1);
-    }else{
-      console.log("remove 1 circle");
+    } else {
+      slotsCircleRecord[circleTeam[indexCircle].slotNum] -= 1;
       circleTeam.splice(indexCircle, 1);
     }
 
-    console.log("cross:" + crossTeam.length + " circle:" + circleTeam.length);
+    console.log("[total players]cross:" + crossTeam.length + " circle:" + circleTeam.length);
   })
 })
 
